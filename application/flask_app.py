91d738dbf86614ce5e8280063c7c84d4834a7da5
Anaëlle <97473758+Viveledelire@users.noreply.github.com>
2024-04-03 10:55:33 +0200

    Update flask_app.py
    abandon des modifications temporairement

--- application/flask_app.py
+++ application/flask_app.py
@@ -1,16 +1,6 @@
 from flask import Flask
-from prometheus_flask_exporter import PrometheusFlaskExporter
-from prometheus_client import Counter
 
 app = Flask(__name__)
-exporter = PrometheusFlaskExporter(app)
-
-@app.route("/metrics")
-def metrics():
-    return exporter.render()
-
-# Metric n°1 Nombre de visites
-nb_visites = Counter("nb_visites", "Nombre de visites sur la page")
 
 @app.route('/')
 def index():
